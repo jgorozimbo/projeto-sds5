38c20448ff2b93de24c13edb94ce3a930e863c4e
Josemar Orozimbo <jgorozimbo@gmail.com>
2021-11-01 09:16:14 -0300

    Projetos criados

---
+++ backend/src/main/java/com/cursostabajara/dsvendas/ProjetoDesenvolvidoDuranteASemanaSpringReactDaDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.cursostabajara.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDesenvolvidoDuranteASemanaSpringReactDaDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDesenvolvidoDuranteASemanaSpringReactDaDevSuperiorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cursostabajara/dsvendas/ProjetoDesenvolvidoDuranteASemanaSpringReactDaDevSuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursostabajara.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDesenvolvidoDuranteASemanaSpringReactDaDevSuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
