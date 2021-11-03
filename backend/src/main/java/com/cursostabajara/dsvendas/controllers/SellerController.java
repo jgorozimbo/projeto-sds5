a86a6cbb33a9f21e5fbc0abe5f12eeef2b89efb4
Josemar Orozimbo <jgorozimbo@gmail.com>
2021-11-03 10:09:04 -0300

    Layers

---
+++ backend/src/main/java/com/cursostabajara/dsvendas/controllers/SellerController.java
@@ -0,0 +1,26 @@
+package com.cursostabajara.dsvendas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursostabajara.dsvendas.dto.SellerDTO;
+import com.cursostabajara.dsvendas.service.SellerService;
+
+@RestController
+@RequestMapping(value = "/sellers")
+public class SellerController {
+
+	@Autowired
+	private SellerService service;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findAll() {
+		List<SellerDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+}
---
+++ backend/src/main/java/com/cursostabajara/dsvendas/service/SellerService.java
@@ -0,0 +1,25 @@
+package com.cursostabajara.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursostabajara.dsvendas.dto.SellerDTO;
+import com.cursostabajara.dsvendas.entities.Seller;
+import com.cursostabajara.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+}
