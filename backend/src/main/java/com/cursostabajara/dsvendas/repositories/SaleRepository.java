581c5b26f2fa04914e81bcee12b969045724602b
Josemar Orozimbo <jgorozimbo@gmail.com>
2021-11-03 21:53:09 -0300

    Pagination

---
+++ backend/src/main/java/com/cursostabajara/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.cursostabajara.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursostabajara.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
